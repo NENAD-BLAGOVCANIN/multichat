0c0abadc631f26577aa321175fc9c8800e1c3c9b
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-06-27 20:32:53 +0200

    updates

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { ReactComponent as ChatIcon } from '../../assets/img/svg/chat.svg'
 
 function Welcome() {
 
@@ -11,8 +12,8 @@ function Welcome() {
         <div className='main-content-wrapper p-0'>
             <div className='d-flex align-items-center justify-content-center' style={{ height: 'calc(100vh - 50px)' }}>
                 <div className='d-flex flex-column align-items-center'>
+                    <p className='mb-2'><ChatIcon /></p>
                     <p className='mb-2'>Start by creating a new tab</p>
-                    <p className='mb-2'><FontAwesomeIcon icon={faPlus} /></p>
                 </div>
             </div>
         </div>
